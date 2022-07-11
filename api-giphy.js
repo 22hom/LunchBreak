1a475e67dca3eb2d9cc77c30cd55776e66899585
rikkerorbak <rikke.rorbak@gmail.com>
2022-07-11 17:24:46 +0200

    new gif every 5 sec

--- api-giphy.js
+++ api-giphy.js
@@ -1,19 +1,24 @@
 const container = document.querySelector('#gif');
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=yDiiFh3IvRqMw8D6Xd5i0dOC4IrnWsBM&tag=lego+food&rating=g');
+const gif = document.createElement('img');
+container.appendChild(gif);
 
-request.onload = function () {
-   var response = request.response;
-   var parsedData = JSON.parse(response);
-   var originalUrl = parsedData.data.images.original.url;
-   console.log(originalUrl);
+const urlTag = 'https://api.giphy.com/v1/gifs/random?api_key=yDiiFh3IvRqMw8D6Xd5i0dOC4IrnWsBM&tag=food&rating=g'
+// const urlSearch = 'https://api.giphy.com/v1/gifs/search?q=food&api_key=yDiiFh3IvRqMw8D6Xd5i0dOC4IrnWsBM' //aner ikke, hvorfor search query ikke virker
 
-   //Create gif on page
-   var gif = document.createElement('img');
-   gif.setAttribute('src', originalUrl);
-   document.body.appendChild(gif);
-   // content.appendChild(gif)
-};
+function getGif() {
+   request.open('GET', urlTag);
+   request.onload = function () {
+      var response = request.response;
+      var parsedData = JSON.parse(response);
+      var originalUrl = parsedData.data.images.original.url;
+      // console.log(originalUrl);
 
-request.send(); 
+      //Create gif on page
+
+      gif.setAttribute('src', originalUrl);
+   };
+   request.send();
+}
+setInterval(getGif, 5000);
