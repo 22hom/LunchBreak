d28752ade60a260ee82f70c994d94f2b7be43546
rikkerorbak <rikke.rorbak@gmail.com>
2022-08-08 22:24:20 +0200

    swapped names, index.js and scraper.js

--- src/scraper.js
+++ src/scraper.js
@@ -1,48 +1,76 @@
-const midtownContent = document.querySelector('#midtown-frame-container');
-const campusContent = document.querySelector('#campus-frame-container');
-const iconArray = ['../src/assets/icons/hot.png', '../src/assets/icons/diet.png', '../src/assets/icons/salad.png'];
 
-async function getMidTownMenu() {   //consider using async/await instead of promises
-   fetch('http://localhost:8000/midtownMenu')
-      .then(response => { return response.json() }) //return response.json()
-      .then(data => {
-         console.log(data);   //check return type
-         if (!data) {
-            console.log('there is no data!');
-            if (midtownContent.childElementCount < 2) {
-               const noMenuMsg = `<div class="menuItem"><h2>Intet på menuen i dag</h2></div>`
-               midtownContent.insertAdjacentHTML('beforeend', noMenuMsg);
-            }
-         } else {
-            data.forEach(item => {
-               const menuItem = `<div class="menuItem"><h3>${item.title}</h3><p>${item.description}</p></div>`
-               midtownContent.insertAdjacentHTML('beforeend', menuItem)
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import express from 'express';
+import cors from 'cors';
+// import './styles.css'
+
+// import { getGif, getGifNoFirstDelay } from './giphy.js';569
+
+const app = express();
+app.use(cors());
+
+const PORT = 8000;
+
+const midtownSite = 'https://lego.isscatering.dk/midtown';
+const campusSite = 'https://lego.isscatering.dk/aastvej';
+
+app.get('/', function (req, res) {
+   res.json('Hello from webscraper');
+})
+
+app.get('/midtownMenu', (req, res) => {
+   axios(midtownSite)
+      .then(response => {
+         const midtownHtml = response.data
+         const midtownMenu = cheerio.load(midtownHtml)
+         const menuItemsArray = []
+
+         midtownMenu('.menu-row.show-description.row', midtownHtml).each(function () {
+            const title = midtownMenu(this).find('.element.title.col-md-12.col-xs-12').text().trim()
+            const description = midtownMenu(this).find('.element.show-description.description.col-md-12.col-xs-12').text().trim()
+
+            menuItemsArray.push({
+               title,
+               description
             })
-         }
-      })
-      .catch(err => console.log(err))
-}
+         })
 
-async function getCampusMenu() {
-   fetch('http://localhost:8000/campusMenu')
-      .then(response => { return response.json() })
-      .then(data => {
-         if (!data) {
-            console.log('There is no data!');
-            if (campusContent.childElementCount < 1) {
-               const noMenuMsg = `<div class="menuItem"><h2>Intet på menuen i dag</h2></div>`
-               campusContent.insertAdjacentHTML('beforeend', noMenuMsg);
-            }
-         } else {
-            data.forEach(item => {
-               const menuItem = `<div class="menuItem"><h3>${item.title}</h3><p>${item.description}</p></div>`
-               campusContent.insertAdjacentHTML('beforeend', menuItem)
+         res.json(menuItemsArray)
+      }).catch(err => console.log(err));
+})
+
+
+
+app.get('/campusMenu', (req, res) => {
+   axios(campusSite)
+      .then(response => {
+         const campusHtml = response.data
+         const campusMenu = cheerio.load(campusHtml)
+         const menuItemsArray = []
+
+         campusMenu('.menu-row.show-description.row', campusHtml).each(function () {
+            const title = campusMenu(this).find('.element.title.col-md-12.col-xs-12').text().trim()
+            const description = campusMenu(this).find('.element.show-description.description.col-md-12.col-xs-12').text().trim()
+            menuItemsArray.push({
+               title,
+               description
             })
-         }
-      })
-      .catch(err => console.log(err));
-}
+         })
 
-getMidTownMenu();
-getCampusMenu();
+         res.json(menuItemsArray);
+      }).catch(err => console.log(err));
+})
 
+
+// const container = document.querySelector('#img-container');
+
+// const gif = document.createElement('img');
+// container.appendChild(gif);
+
+// const container = document.querySelector('#img-container');
+// const gif = document.createElement('img');
+// container.appendChild(gif);
+// getGifNoFirstDelay(getGif, 10000);
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
